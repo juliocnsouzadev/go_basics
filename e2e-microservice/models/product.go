ca00bd479ae13c2b831f55f73c3a1889ec305ac5
juliocnsouzadev <juliocnsouzadev@gmail.com>
2022-02-14 16:55:13 -0300

    listing products

---
+++ e2e-microservice/models/product.go
@@ -0,0 +1,12 @@
+package models
+
+type Product struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
